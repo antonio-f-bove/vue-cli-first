0e1ae0d58935bbaa7303fcb6d321e96a27728068
AntonioFBove <antoniofbove@gmail.com>
2022-01-27 10:21:10 +0100

    project restored

---
+++ src/components/Title.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    La mia prima app con Vue CLI
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  div {
+    text-align: center;
+    font-size: 30px;
+    font-weight: bold;
+  }
+</style>
